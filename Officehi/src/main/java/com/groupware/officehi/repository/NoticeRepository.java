e88a1444299cab8e5d6180f4b584d6a48df86838
bnanadog <vebmisesk@gmail.com>
2023-12-28 09:53:40 +0900

    공지사항 페이징

--- Officehi/src/main/java/com/groupware/officehi/repository/NoticeRepository.java
+++ Officehi/src/main/java/com/groupware/officehi/repository/NoticeRepository.java
@@ -6,28 +6,32 @@ import java.util.Optional;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.groupware.officehi.domain.Paging;
 import com.groupware.officehi.dto.NoticeDTO;
 
 /**
  * @author 정유진
  * @editDate 23.12.18 ~ 23.12.19
  * 
- * @author 이승준
- * 검색 기능 추가 23.12.24 ~ 23.12.26
+ * @author 이승준 검색 기능 추가 23.12.24 ~ 23.12.26
  */
 
 @Repository
 public interface NoticeRepository {
-	
+
 	void insert(NoticeDTO notice);
-	
+
 	List<NoticeDTO> findAll();
-	
+
 	Optional<NoticeDTO> findByNoticeNo(Long noticeNo);
+	
+	List<NoticeDTO> findNoticePaging(Paging paging);
 	
 	/* 검색 기능 */
 	List<NoticeDTO> searchTitle(@Param("title") String title);
+
 	List<NoticeDTO> searchContent(@Param("content") String content);
+
 	List<NoticeDTO> searchNoticeNo(@Param("noticeNo") Long noticeNo);
-	
+
 }
--- Officehi/src/main/java/com/groupware/officehi/service/NoticeService.java
+++ Officehi/src/main/java/com/groupware/officehi/service/NoticeService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.groupware.officehi.domain.Paging;
 import com.groupware.officehi.dto.NoticeDTO;
 import com.groupware.officehi.repository.NoticeRepository;
 
@@ -14,36 +15,40 @@ import lombok.RequiredArgsConstructor;
  * @author 정유진
  * @editDate 23.12.18 ~ 23.12.19
  * 
- * @author 이승준
- * 검색 기능 추가 23.12.24 ~ 23.12.26
+ * @author 이승준 검색 기능 추가 23.12.24 ~ 23.12.26
  */
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
-	
+
 	private final NoticeRepository noticeRepository;
-	
+
 	public void insertNotice(NoticeDTO notice) {
 		noticeRepository.insert(notice);
 	}
-	public List<NoticeDTO> findAll(){
+
+	public List<NoticeDTO> findAll() {
 		return noticeRepository.findAll();
 	}
-	
-	public Optional<NoticeDTO> findByNoticeNo(Long noticeNo){
+
+	public Optional<NoticeDTO> findByNoticeNo(Long noticeNo) {
 		return noticeRepository.findByNoticeNo(noticeNo);
 	}
 	
+	public List<NoticeDTO> findNoticePaging(Paging paging){
+		return noticeRepository.findNoticePaging(paging);
+	}
+
 	/* 검색 기능 */
 	public List<NoticeDTO> searchTitle(String title) {
 		return noticeRepository.searchTitle(title);
 	}
-	
+
 	public List<NoticeDTO> searchContent(String content) {
 		return noticeRepository.searchContent(content);
 	}
-	
+
 	public List<NoticeDTO> searchNoticeNo(Long noticeNo) {
 		return noticeRepository.searchNoticeNo(noticeNo);
 	}
